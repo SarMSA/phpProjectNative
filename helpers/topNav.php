ed852c36ff900a5af128d4586ae1ff9773789b0b
SarMSA <86629729+SarMSA@users.noreply.github.com>
2021-10-23 00:21:19 +0200

    uptaded

--- helpers/topNav.php
+++ helpers/topNav.php
@@ -10,18 +10,18 @@
 					<div class="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul class="navbar-nav ml-auto">
 						<li class="nav-item <?php echo $home??NULL;?>">
-						<a class="nav-link" href="<?php echo hurl('index.php')?>">Home <span class="sr-only">(current)</span></a>
+						<a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
 						</li>
 						<li class="nav-item <?php echo $shopping??NULL;?>">
-						<a class="nav-link" href="<?php echo hurl('shopping.php')?>">Shopping</a>
+						<a class="nav-link" href="shopping.php">Shopping</a>
 						</li>
 						<li class="nav-item <?php echo $signIn??NULL;?>">
 							<?php if (!isset($_SESSION['user']) and !isset($_SESSION['admin'])) {?>
 								<span class="nav-link" id="button-signIn">Sign In <i class="fas fa-user"></i></span>
 							<?php }elseif (isset($_SESSION['user']) and !isset($_SESSION['admin'])) {?>
-								<a class="nav-link" href="<?php echo hurl('user/index.php')?>">My account <i class="fas fa-user-circle" style="font-size: 1.3rem;"></i></a>
+								<a class="nav-link" href="<?php echo urlUser('index.php')?>">My account <i class="fas fa-user-circle" style="font-size: 1.3rem;"></i></a>
 							<?php }elseif (!isset($_SESSION['user']) and isset($_SESSION['admin'])) { ?>
-								<a class="nav-link" href="<?php echo hurl('admin/index.php')?>">ADMIN <i class="fas fa-user-cog"></i></a>
+								<a class="nav-link" href="<?php echo url('index.php')?>">ADMIN <i class="fas fa-user-cog"></i></a>
 							<?php }?>
 						</li>
 						<li class="nav-item <?php echo $cert??NULL;?>" style=" <?php if (isset($_SESSION['admin'])){echo 'display: none;';} ?>">
