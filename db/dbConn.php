46e70c0aef8d53187070936930f0ab19c66ae398
SarMSA <86629729+SarMSA@users.noreply.github.com>
2021-10-22 22:53:23 +0200

    uptaded

--- db/dbConn.php
+++ db/dbConn.php
@@ -5,22 +5,22 @@ session_start();
 // $dbPassword  = '';
 // $dbName = 'shopping';
 
-// $server = 'us-cdbr-east-04.cleardb.com';
-// $dbUser = 'bb2a1ae18e4b04';
-// $dbPassword  = 'fdeac4f6';
-// $dbName = 'heroku_9fec70058342bef';
+$server = 'us-cdbr-east-04.cleardb.com';
+$dbUser = 'bb2a1ae18e4b04';
+$dbPassword  = 'fdeac4f6';
+$dbName = 'heroku_9fec70058342bef';
 
-// $conn = mysqli_connect($server, $dbUser, $dbPassword, $dbName);
+$conn = mysqli_connect($server, $dbUser, $dbPassword, $dbName);
 // 
-$cleardb_url = parse_url(getenv("heroku_9fec70058342bef"));
-$cleardb_server = $cleardb_url["us-cdbr-east-04.cleardb.com"];
-$cleardb_username = $cleardb_url["bb2a1ae18e4b04"];
-$cleardb_password = $cleardb_url["fdeac4f6"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $cleardb_server = "us-cdbr-east-04.cleardb.com";
+// $cleardb_username = "bb2a1ae18e4b04";
+// $cleardb_password = "fdeac4f6";
+// $cleardb_db = "heroku_9fec70058342bef";
+// $active_group = 'default';
+// $query_builder = TRUE;
+// // Connect to DB
+// $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 if(!($conn)) 
 {
     die("error: ".mysqli_connect_error());
