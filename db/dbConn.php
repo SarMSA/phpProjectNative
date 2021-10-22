768a45c469f6a15c16503d948142435f05964fd0
SarMSA <86629729+SarMSA@users.noreply.github.com>
2021-10-22 21:32:06 +0200

    connect to heroku

--- db/dbConn.php
+++ db/dbConn.php
@@ -1,11 +1,21 @@
 <?php
 session_start();
-$server = 'localhost';
-$dbUser = 'root';
-$dbPassword  = '';
-$dbName = 'shopping';
+// $server = 'localhost';
+// $dbUser = 'root';
+// $dbPassword  = '';
+// $dbName = 'shopping';
 
-$conn = mysqli_connect($server, $dbUser, $dbPassword, $dbName);
+// $conn = mysqli_connect($server, $dbUser, $dbPassword, $dbName);
+// 
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 if(!($conn)) 
 {
     die("error: ".mysqli_connect_error());
